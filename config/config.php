e1d1477fac48856ba695803631f215774750349b
devplanete <devplanete@free.fr>
2014-04-19 15:15:25 +0200

    Remember me from multiple devices

--- config/config.php
+++ config/config.php
@@ -23,18 +23,19 @@ define("DB_PASS", "mysql");
  * Please note: The COOKIE_DOMAIN needs the domain where your app is,
  * in a format like this: .mydomain.com
  * Note the . in front of the domain. No www, no http, no slash here!
- * For local development .127.0.0.1 or .localhost is fine, but when deploying you should
- * change this to your real domain, like '.mydomain.com' ! The leading dot makes the cookie available for
- * sub-domains too.
+ * For local development, use false because .127.0.0.1 or .localhost don't work inside Chrome 
+ * but when deploying you should change this to your real domain, like '.mydomain.com' !
+ * The leading dot makes the cookie available for sub-domains too.
  * @see http://stackoverflow.com/q/9618217/1114320
  * @see http://www.php.net/manual/en/function.setcookie.php
+ * @see http://stackoverflow.com/questions/1134290/cookies-on-localhost-with-explicit-domain
  *
  * COOKIE_RUNTIME: How long should a cookie be valid ? 1209600 seconds = 2 weeks
  * COOKIE_DOMAIN: The domain where the cookie is valid for, like '.mydomain.com'
  * COOKIE_SECRET_KEY: Put a random value here to make your app more secure. When changed, all cookies are reset.
  */
 define("COOKIE_RUNTIME", 1209600);
-define("COOKIE_DOMAIN", ".127.0.0.1");
+define("COOKIE_DOMAIN", false); // to be replaced by '.mydomain.com'
 define("COOKIE_SECRET_KEY", "1gp@TMPS{+$78sfpMJFe-92s");
 
 /**
