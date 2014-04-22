ca7928bd012d0a337f29e0f841e091db50cde319
devplanete <devplanete@free.fr>
2014-04-22 18:30:00 +0200

    Merge Registration class into Login class

--- views/_header.php
+++ views/_header.php
@@ -1,31 +1,39 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <title>php-login-advanced</title>
-    <style type="text/css">
-        /* just for the demo */
-        body {
-            font-family: Arial, sans-serif;
-            font-size: 12px;
-            margin: 10px;
-        }
-        label {
-            position: relative;
-            vertical-align: middle;
-            bottom: 1px;
-        }
-        input[type=text],
-        input[type=password],
-        input[type=submit],
-        input[type=email] {
-            display: block;
-            margin-bottom: 15px;
-        }
-        input[type=checkbox] {
-            margin-bottom: 15px;
-        }
-    </style>
+	<meta charset="UTF-8">
+	<title>php-login-advanced</title>
+	<style type="text/css">
+	/* just for the demo */
+	body {
+		font-family: Arial, sans-serif;
+		font-size: 12px;
+		margin: 10px;
+	}
+	label {
+		position: relative;
+		vertical-align: middle;
+		bottom: 1px;
+	}
+	input[type=text],
+	input[type=password],
+	input[type=submit],
+	input[type=email] {
+		display: block;
+		margin-bottom: 15px;
+	}
+	input[type=checkbox] {
+		margin-bottom: 15px;
+	}
+	.login_error {
+		background: #f36971;
+		padding: 2px;
+	}
+	.login_message {
+		background: #99daea;
+		padding: 2px;
+	}
+	</style>
 </head>
 <body>
 
@@ -34,28 +42,12 @@
 if (isset($login)) {
     if ($login->errors) {
         foreach ($login->errors as $error) {
-            echo $error;
+            echo "<div class=\"login_error\">$error</div>\n";
         }
     }
     if ($login->messages) {
         foreach ($login->messages as $message) {
-            echo $message;
-        }
-    }
-}
-?>
-
-<?php
-// show potential errors / feedback (from registration object)
-if (isset($registration)) {
-    if ($registration->errors) {
-        foreach ($registration->errors as $error) {
-            echo $error;
-        }
-    }
-    if ($registration->messages) {
-        foreach ($registration->messages as $message) {
-            echo $message;
+            echo "<div class=\"login_message\">$message</div>\n";
         }
     }
 }
