409a1e8005d364d12986365df98a9b46afea8af5
devplanete <devplanete@free.fr>
2014-04-22 18:29:12 +0200

    Merge Registration class into Login class

--- register.php
+++ register.php
@@ -3,9 +3,9 @@
 // load php-login components
 require_once("php-login.php");
 
-// create the registration object. when this object is created, it will do all registration stuff automatically
+// create the login object. When this object is created, it will do all registration stuff automatically
 // so this single line handles the entire registration process.
-$registration = new Registration();
+$login = new Login();
 
 // showing the register view (with the registration form, and messages/errors)
 include("views/register.php");
