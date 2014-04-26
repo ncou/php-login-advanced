5d8a11e2d0400938fcb0df9149669de86d598124
devplanete <devplanete@free.fr>
2014-04-26 18:43:09 +0200

    unique php file entry point

--- views/logged_in.php
+++ views/logged_in.php
@@ -1,4 +1,3 @@
-<?php include('_header.php'); ?>
 
 <?php
 // if you need the user's information, just put them into the $_SESSION variable and output them here
@@ -7,8 +6,6 @@ echo WORDING_PROFILE_PICTURE . '<br/><img src="' . $login->getGravatarImageUrl()
 ?>
 
 <div>
-    <a href="index.php?logout"><?php echo WORDING_LOGOUT; ?></a>
-    <a href="edit.php"><?php echo WORDING_EDIT_USER_DATA; ?></a>
+	<a href="?logout"><?php echo WORDING_LOGOUT; ?></a>
+	<a href="?edit"><?php echo WORDING_EDIT_USER_DATA; ?></a>
 </div>
-
-<?php include('_footer.php'); ?>
