23c59d6345ca58ea2d98f7220b2cb9be7bdec5a7
devplanete <devplanete@free.fr>
2014-04-26 18:45:47 +0200

    unique php file entry point

--- index.php
+++ index.php
@@ -1,30 +1,45 @@
 <?php
-
 /**
  * A simple PHP Login Script / ADVANCED VERSION
- * For more versions (one-file, minimal, framework-like) visit http://www.php-login.net
  *
- * @author Panique
- * @link http://www.php-login.net
- * @link https://github.com/panique/php-login-advanced/
+ * @link https://github.com/devplanete/php-login-advanced
  * @license http://opensource.org/licenses/MIT MIT License
  */
 
 // load php-login components
 require_once("php-login.php");
-
-// create a login object. when this object is created, it will do all login/logout stuff automatically
+// the login object will do all login/logout stuff automatically
 // so this single line handles the entire login process.
 $login = new Login();
 
-// ... ask if we are logged in here:
-if ($login->isUserLoggedIn() == true) {
-    // the user is logged in. you can do whatever you want here.
-    // for demonstration purposes, we simply show the "you are logged in" view.
-    include("views/logged_in.php");
+include('views/_header.php');
 
+// show the registration form
+if (isset($_GET['register']) && ! $login->isRegistrationSuccessful()) {
+    include('views/register.php');
+
+// show the request-a-password-reset or type-your-new-password form
+} else if (isset($_GET['password_reset']) && ! $login->isPasswordResetSuccessful()) {
+    if (isset($_REQUEST['user_name']) && isset($_REQUEST['verification_code']) && $login->isPasswordResetLinkValid()) {
+        // reset link is correct: ask for the new password
+        include("views/password_reset.php");
+    } else {
+        // no data from a password-reset-mail has been provided, 
+        // we show the request-a-password-reset form
+        include('views/password_reset_request.php');
+    }
+
+// show the edit form to modify username, email or password
+} else if (isset($_GET['edit']) && $login->isUserLoggedIn()) {
+    include('views/edit.php');
+
+// the user is logged in, we show informations about the current user
+} else if ($login->isUserLoggedIn()) {
+    include('views/logged_in.php');
+
+// the user is not logged in, we show the login form
 } else {
-    // the user is not logged in. you can do whatever you want here.
-    // for demonstration purposes, we simply show the "you are not logged in" view.
-    include("views/not_logged_in.php");
+    include('views/login.php');
 }
+
+include('views/_footer.php');
