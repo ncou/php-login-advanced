2bf37d66ccf019b8f938e83b64d026513f48b82f
austinkregel <austin@kbcomp.co>
2014-12-27 00:24:53 -0500

    deleted:    composer.json 	deleted:    index.php
     #Changes to be committed:
    	deleted:    composer.json
    	deleted:    index.php
    	_installation/
    	classes/
    	composer.json
    	config/
    	index.php
    	libraries/
    	tools/
    	translations/
    	views/
    
    	new file:   _installation/01-create-database.sql
    	new file:   _installation/02-create-user-table.sql
    	new file:   _installation/03-create-user_connections-table.sql
    	new file:   classes/PHPLogin.php
    	new file:   classes/PHPMailer.php
    	new file:   classes/SMTP.php
    	modified:   composer.json
    	new file:   config/autoload.php
    	new file:   config/config.php
    	modified:   index.php
    	new file:   libraries/password_compatibility_library.php
    	new file:   tools/fonts/times_new_yorker.ttf
    	new file:   tools/showCaptcha.php
    	new file:   translations/en.php
    	new file:   translations/fr.php
    	new file:   translations/ru.php
    	new file:   views/.htaccess
    	new file:   views/_footer.php
    	new file:   views/_header.php
    	new file:   views/edit.php
    	new file:   views/error.png
    	new file:   views/info.png
    	new file:   views/logged_in.php
    	new file:   views/login.php
    	new file:   views/password_reset.php
    	new file:   views/password_reset_request.php
    	new file:   views/register.php

--- index.php
+++ index.php
@@ -5,42 +5,11 @@
  * @link https://github.com/devplanete/php-login-advanced
  * @license http://opensource.org/licenses/MIT MIT License
  */
-
-// load php-login class
-require_once("PHPLogin.php");
+  $dir = dirname( __FILE__ );
+// Load application config (error reporting, database credentials etc.)
+require $dir . '/config/config.php';
+// The auto-loader to load the php-login related internal stuff automatically
+require $dir .'/config/autoload.php';
+// The Composer auto-loader (official way to load Composer contents) to load external stuff automatically
 // the login object will do all login/logout stuff automatically
 // so this single line handles the entire login process.
-$login = new PHPLogin();
-
-include('views/_header.php');
-
-// show the registration form
-if (isset($_GET['register']) && ! $login->isRegistrationSuccessful() && 
-   (ALLOW_USER_REGISTRATION || (ALLOW_ADMIN_TO_REGISTER_NEW_USER && $_SESSION['user_access_level'] == 255))) {
-    include('views/register.php');
-
-// show the request-a-password-reset or type-your-new-password form
-} else if (isset($_GET['password_reset']) && ! $login->isPasswordResetSuccessful()) {
-    if (isset($_REQUEST['user_name']) && isset($_REQUEST['verification_code']) && $login->isPasswordResetLinkValid()) {
-        // reset link is correct: ask for the new password
-        include("views/password_reset.php");
-    } else {
-        // no data from a password-reset-mail has been provided, 
-        // we show the request-a-password-reset form
-        include('views/password_reset_request.php');
-    }
-
-// show the edit form to modify username, email or password
-} else if (isset($_GET['edit']) && $login->isUserLoggedIn()) {
-    include('views/edit.php');
-
-// the user is logged in, we show informations about the current user
-} else if ($login->isUserLoggedIn()) {
-    include('views/logged_in.php');
-
-// the user is not logged in, we show the login form
-} else {
-    include('views/login.php');
-}
-
-include('views/_footer.php');
