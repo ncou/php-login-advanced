2ebe218b45b8c430e0b7436f068226148cc1d958
Austin <austinkregel@users.noreply.github.com>
2015-04-19 23:32:42 -0400

    Update logged_in.php

--- views/logged_in.php
+++ views/logged_in.php
@@ -1,6 +1,6 @@
 
-<fieldset>
-	<legend><?php echo WORDING_EDIT_USER_DATA; ?></legend>
+<div>
+	<h2><?php echo WORDING_EDIT_USER_DATA; ?></h2>
 	<p><?php echo WORDING_YOU_ARE_LOGGED_IN_AS . '<b>' . $_SESSION['user_name']; ?></b></p><hr/>
 
 	<p>$_SESSION['user_id'] = <?php echo $_SESSION['user_id']; ?></p>
@@ -10,6 +10,6 @@
 	<p>$_SESSION['user_logged_in'] = <?php echo $_SESSION['user_logged_in']; ?></p><hr/>
 
 	<p><?php echo WORDING_PROFILE_PICTURE . '<br/><img src="' . $login->getGravatarImageUrl() ; ?>" /></p>
-</fieldset><br/>
+</div><br/>
 <a href="?logout"><?php echo WORDING_LOGOUT; ?></a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="?edit"><?php echo WORDING_EDIT_USER_DATA; ?></a>
 <?php echo (ALLOW_ADMIN_TO_REGISTER_NEW_USER && $_SESSION['user_access_level'] == 255 ? '<br/><a href="?register">'. WORDING_REGISTER_NEW_ACCOUNT .'</a>' : ''); ?>
