a826faa0ad8b7339674d2ef7be1039ca36a47585
Austin <austinkregel@users.noreply.github.com>
2015-04-20 12:39:30 -0400

    Update LibraryTest.php

--- tests/Module/LibraryTest.php
+++ tests/Module/LibraryTest.php
@@ -5,7 +5,7 @@ class LibraryTest extends PHPUnit_Framework_TestCase
   public function testLibrary()
   {
     $login = new PHPLogin();
-    $this->assertEquals($login->isUserLoggedIn(),false)
+    $this->assertEquals($login->isUserLoggedIn(),false);
     
   } 
 }
