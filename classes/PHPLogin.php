a8a412a9f3564d8306ca7ebccd889cbcf6f69d2d
Austin <austinkregel@users.noreply.github.com>
2015-05-30 23:26:39 -0400

    Fixed require error
    The require error prevented the pages to render

--- classes/PHPLogin.php
+++ classes/PHPLogin.php
@@ -50,7 +50,7 @@ class PHPLogin{
         } else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
             // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
             // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
-            @require_once(dirname( __FILE__ ).'../libraries/password_compatibility_library.php');
+            require_once(dirname( __FILE__ ).'/../libraries/password_compatibility_library.php');
         }
 
         // include the config
@@ -59,7 +59,7 @@ class PHPLogin{
         // detection of the language for the current user/browser
         $user_lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
         // if translation file for the detected language doesn't exist, we use default english file
-        @require_once(dirname( __FILE__ ).'../translations/' . (file_exists(dirname( __FILE__ ).'/../translations/' . $user_lang . '.php') ? $user_lang : 'en') . '.php');
+        require_once(dirname( __FILE__ ).'/../translations/' . (file_exists(dirname( __FILE__ ).'/../translations/' . $user_lang . '.php') ? $user_lang : 'en') . '.php');
 
         // create/read session
         @session_start();
