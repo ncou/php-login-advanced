ad0b51e0ab4c03d1b684c1e78891d60dd9d736c1
Austin <austinkregel@users.noreply.github.com>
2015-07-06 14:28:50 -0400

    Update password_reset_request.php

--- views/password_reset_request.php
+++ views/password_reset_request.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <div class="col-md-4 col-md-offset-4">
 	<form method="post" action="?password_reset">
 		<fieldset>
@@ -11,13 +10,3 @@
 	</form>
 	<a href="<?php echo $_SERVER['SCRIPT_NAME']; ?>"><?php echo WORDING_BACK_TO_LOGIN; ?></a>
 </div>
-=======
-<h2><?php echo WORDING_FORGOT_MY_PASSWORD; ?></h2>
-<form method="post" action="?password_reset">
-	<label for="user_name"><?php echo WORDING_REQUEST_PASSWORD_RESET; ?></label>
-	<input id="user_name" type="text" name="user_name" required autofocus/>
-	<input type="submit" name="request_password_reset" value="<?php echo WORDING_RESET_PASSWORD; ?>" />
-</form>
-
-<a href="<?php echo $_SERVER['SCRIPT_NAME']; ?>"><?php echo WORDING_BACK_TO_LOGIN; ?></a>
->>>>>>> bugfixes
