9ffcbc5b75648bc0c18fd2b9cd74f23b1c43d7f0
Austin <austinkregel@users.noreply.github.com>
2015-08-10 21:26:40 -0400

    Update register.php

--- views/register.php
+++ views/register.php
@@ -1,10 +1,10 @@
 <h2><?php echo WORDING_REGISTER; ?></h2>
 <form method="post" action="?register">
 	<label for="user_name"><?php echo WORDING_REGISTRATION_USERNAME; ?></label>
-	<input id="user_name" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" value="<?php echo (isset($_POST['user_name']) ? $_POST['user_name'] : ''); ?>" required autofocus/>
+	<input id="user_name" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" value="<?php echo (isset($_POST['user_name']) ? htmlspecialchars($_POST['user_name']) : ''); ?>" required autofocus/>
 
 	<label for="user_email"><?php echo WORDING_REGISTRATION_EMAIL; ?></label>
-	<input id="user_email" type="email" name="user_email" value="<?php echo (isset($_POST['user_email']) ? $_POST['user_email'] : ''); ?>" required />
+	<input id="user_email" type="email" name="user_email" value="<?php echo (isset($_POST['user_email']) ? htmlspecialchars($_POST['user_email']) : ''); ?>" required />
 
 	<label for="user_password_new"><?php echo WORDING_REGISTRATION_PASSWORD; ?></label>
 	<input id="user_password_new" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />
